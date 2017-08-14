db6b34b7cbd47b74166861a10f19a87e419c5ec1
KarthickCSK <karthick16695@gmail.com>
2017-08-15 02:47:53 +0530

    added client side

---
+++ webclient/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { StartComponent } from './component/start/start.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+
+const appRoutes : Routes = [
+  { 
+    path: '', 
+    component: HomeComponent
+  }
+];
+
+@NgModule({
+  declarations: [],
+  imports: [ RouterModule.forRoot(appRoutes) ],
+  providers: [],
+  bootstrap: [  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
---
+++ webclient/src/app/app.module.ts
@@ -0,0 +1,25 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { StartComponent } from './component/start/start.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { HomeComponent } from './component/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StartComponent,
+    NavbarComponent,
+    HomeComponent,
+  ],
+  imports: [
+      BrowserModule,
+      FormsModule,
+      AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
